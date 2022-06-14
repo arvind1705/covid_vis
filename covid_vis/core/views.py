7b6ba7e24c09ca5b3917a8ef9dfc6669ff2140c0
Aravind G <g.aravind4@gmail.com>
2022-06-14 14:14:04 +0000

    Add dynamic data for charts.

--- covid_vis/core/views.py
+++ covid_vis/core/views.py
@@ -1,7 +1,45 @@
 # Create your views here.
+from django.db.models import Count
+from django.db.models.functions import TruncDay
 from django.shortcuts import render
+
+from .models import Patient
 
 
 # views.py
 def index(request):
-    return render(request, "index.html")
+    covid_positive_count_by_day = (
+        Patient.objects.annotate(day=TruncDay("created_at"))
+        .values("day")
+        .annotate(c=Count("covid_positive"))
+        .values("day", "c")
+    )
+    deceased_count_by_day = (
+        Patient.objects.annotate(day=TruncDay("updated_at"))
+        .values("day")
+        .annotate(c=Count("deceased"))
+        .values("day", "c")
+    )
+    recovered_count_by_day = (
+        Patient.objects.annotate(day=TruncDay("updated_at"))
+        .values("day")
+        .annotate(c=Count("recovered"))
+        .values("day", "c")
+    )
+
+    data = {}
+
+    data.update({"covid_positive": get_label_data(covid_positive_count_by_day)})
+    data.update({"deceased": get_label_data(deceased_count_by_day)})
+    data.update({"recovered": get_label_data(recovered_count_by_day)})
+
+    return render(request, "index.html", data)
+
+
+def get_label_data(data):
+    labels = []
+    datas = []
+    for day in data:
+        labels.append(day["day"].strftime("%d"))
+        datas.append(day["c"])
+    return {"labels": labels, "data": datas, "sum": sum(datas)}
