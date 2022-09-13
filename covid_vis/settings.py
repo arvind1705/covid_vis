de8dc8d1e2ec599b4ba3cefd5a99e1adc966e737
Aravind G <g.aravind4@gmail.com>
2022-09-13 18:58:02 +0530

    Update settings.py

--- covid_vis/settings.py
+++ covid_vis/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = "covid_vis.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(BASE_DIR, "template")],
+        "DIRS": [os.path.join(BASE_DIR, "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -127,7 +127,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = "/statics/"
+STATIC_URL = "/static/"
 
 django_heroku.settings(locals())
 
