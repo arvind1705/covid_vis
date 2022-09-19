6bbcb2a46c64e3a424ca20aa84376f54731113d1
Aravind G <g.aravind4@gmail.com>
2022-09-19 10:02:15 +0000

    Update table padding.

--- covid_vis/core/views.py
+++ covid_vis/core/views.py
@@ -48,7 +48,7 @@ class HospitalTable(tables.Table):
         for the row from the table data.
         """
         return format_html(
-            '<a href="{}">{}</a>',
+            '<a style = "margin: 0;" href="{}">{}</a>',
             reverse("hospital_detail", kwargs={"hospital_id": record.id}),
             record.name,
         )
