dc3f63f6610c0f435ad2bd307b1d9b814fc62eee
Aravind G <g.aravind4@gmail.com>
2022-10-14 07:00:19 +0000

    Add new database.

--- covid_vis/settings.py
+++ covid_vis/settings.py
@@ -83,13 +83,24 @@ WSGI_APPLICATION = "covid_vis.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "paoiyfkf",
+        "USER": "paoiyfkf",
+        # trunk-ignore(gitleaks/generic-api-key)
+        "PASSWORD": "KTSo_3VoKBB8YF9F_cqm3115C2Nu75bn",
+        "HOST": "mouse.db.elephantsql.com",
+        "PORT": "5432",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
