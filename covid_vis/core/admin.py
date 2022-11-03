81ac2714629e9c85adfd8e7b84e17e3fa0b0102b
Aravind G <g.aravind4@gmail.com>
2022-11-03 14:17:21 +0530

    Update admin.py

--- covid_vis/core/admin.py
+++ covid_vis/core/admin.py
@@ -14,9 +14,10 @@ class HospitalAdmin(admin.ModelAdmin):
 
 
 class PersonAdmin(admin.ModelAdmin):
-    list_display = ("name", "created_by", "created_at", "updated_at")
+    list_display = ("id", "name", "created_by", "created_at", "updated_at")
     exclude = ("created_by",)
     actions = None
+    search_fields = ['id', ]
 
     def get_queryset(self, request):
         qs = super(PersonAdmin, self).get_queryset(request)
